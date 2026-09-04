7c70da8550c1d17496c1da27f84166f2d1d4b135
Joao Pedro <jpjoaopedrok11@gmail.com>
2026-09-03 23:06:35 -0300

    entrada e saida

---
+++ C.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float a = 23.5f;
+	printf("the value of a is: %f\n", a);
+	return 0;
+}
+
+#include <stdio.h> 
+int main(){
+
+    char characte;
+ float valor1, valor2;
+ printf("enter a d: ");
+ scanf(" %c", &character);
+ printf("enter two float values: ");
+ scanf("%f %f", &valor1, &valor2);
+
+ return 0;
+}
